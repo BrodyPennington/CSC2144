7a5c49894ac41844ca17a3358a58a6c1a21aa2bd
YeahimBrody <brodygpennington@gmail.com>
2023-09-02 13:15:00 -0500

    first

---
+++ Lab1.cpp
@@ -0,0 +1,11 @@
+// Name: Brody Pennington
+// Lab: Lab 1
+// Due Date: 9/7/2023
+// Filename: Lab1.cpp
+// Description: Using functions to convert time from a 24 hour notation to 12 hour notation.
+
+#include <iostream>
+
+int main() {
+    
+}
