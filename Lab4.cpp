39801acfb1fa448b7c4c96e09f306b75be1c1138
YeahimBrody <brodygpennington@gmail.com>
2023-10-02 18:55:24 -0500

    needs comments

--- Lab4.cpp
+++ Lab4.cpp
@@ -17,16 +17,17 @@ struct Computer {
     double price;
 };
 
-Computer readInput(Computer& c1) {
+void readInput(Computer& c1) {
 
     std::cout << "Enter the name of the manufacturer: ";
-    std::cin >> c1.manfac;
+    std::getline(std::cin, c1.manfac);
 
     std::cout << "Enter the model of the computer: ";
     std::cin >> c1.model;
   
     std::cout << "Enter processor type: ";
-    std::cin >> c1.cpu;
+    std::cin.ignore();
+    std::getline(std::cin, c1.cpu);
   
     std::cout << "Enter the size of RAM (in GB): ";
     std::cin >> c1.ram;
@@ -39,30 +40,26 @@ Computer readInput(Computer& c1) {
 
     std::cout << "Enter the price: ";
     std::cin >> c1.price;
+
 }
 
-Computer sendOutput(Computer& c2) {
+void sendOutput(Computer c1) {
 
-    readInput(c2);
-
-
-    std::cout << "Manufacturer: " << c2.manfac << std::endl;
-    std::cout << "Model: " << c2.model << std::endl;
-    std::cout << "Processor: " << c2.cpu << std::endl;
-    std::cout << "Ram: " << c2.ram << "gb" << std::endl;
-    std::cout << "Hard Drive Size: " << c2.drive << "gb" << std::endl;
-    std::cout << "Year Built: " << c2.year_built << std::endl;
-    std::cout << "Price: $" << c2.price << std::endl;
+    std::cout << "Manufacturer: " << c1.manfac << std::endl;
+    std::cout << "Model: " << c1.model << std::endl;
+    std::cout << "Processor: " << c1.cpu << std::endl;
+    std::cout << "Ram: " << c1.ram << "gb" << std::endl;
+    std::cout << "Hard Drive Size: " << c1.drive << "gb" << std::endl;
+    std::cout << "Year Built: " << c1.year_built << std::endl;
+    std::cout << "Price: $" << std::fixed << std::setprecision(2) << c1.price << std::endl;
 
 }
 
 int main() {
     Computer c1;
+
     readInput(c1);
-
-    Computer c2;
-    sendOutput(c2);
-
+    sendOutput(c1);
 
     return 0;
 }
